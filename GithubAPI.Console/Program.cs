6d30b9d72d5d1d9f1d86399a83cabf058c2fd744
suny-am <visualarea.1@gmail.com>
2024-04-12 20:21:40 +0200

    Excise Azure related content

--- GithubAPI.Console/Program.cs
+++ GithubAPI.Console/Program.cs
@@ -20,26 +20,4 @@ var graphQLHandler = new GraphQLHandler(ghApiToken!);
 
 IEnumerable<Repository>? result = await graphQLHandler.GetDiskUsageBySearch();
 
-if (result is null)
-{
-    io.Write("no data found", newline: true);
-}
-else
-{
-    try
-    {
-        CosmosDbHandler cosmosDbHandler = new();
-        await cosmosDbHandler.LoadDatabase();
-
-        foreach (var repo in result)
-        {
-            io.Write($"Creating entry for repository {repo.name}...", newline: true);
-            await cosmosDbHandler.Create(repo);
-            io.WriteEncoded($"[green]{repo.name} inserted successfully into container![green]{Environment.NewLine}");
-        }
-    }
-    catch (Exception ex)
-    {
-        throw new Exception(ex.Message);
-    }
-}
+io.Write(result.Count(), newline: true)
